e5f16f245d66baa2980ddce21d2954afda02f090
oca <codeconnects@gmail.com>
2020-11-02 11:25:41 -0500

    Add assorted button components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import NewButton from './components/NewButton';
+import SaveButton from './components/SaveButton';
+import ClearButton from './components/ClearButton';
+import DeleteButton from './components/DeleteButton';
+import LoadButton from './components/LoadButton';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    
+      <Header />
+
+      <section id="primary-content" className="sticky-wrap">
+
+        <NewButton id="new-btn" className="dash-btn" text="+ New Sticky" />
+        <SaveButton id="save-btn" className="dash-btn" text="Save Board" />
+        <ClearButton id="clear-btn" className="dash-btn" text="Clear All" />
+        <DeleteButton id="delete-btn" className="dash-btn" text="Delete All" />
+        <LoadButton id="load-btn" className="dash-btn" text="Load Board" />
+
+        <div id="sticky-notes"></div>
+      </section>
+
     </div>
   );
 }
