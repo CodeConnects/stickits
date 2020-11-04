5fbac6d15b7914fff3c813cccec68e7a9116819c
oca <codeconnects@gmail.com>
2020-11-04 12:27:45 -0500

    created separate noteboard component

--- src/App.js
+++ src/App.js
@@ -3,31 +3,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import NewButton from './components/NewButton';
-import SaveButton from './components/SaveButton';
-import ClearButton from './components/ClearButton';
-import DeleteButton from './components/DeleteButton';
-import LoadButton from './components/LoadButton';
-
+import NoteBoard from './components/NoteBoard';
 
 function App() {
 
   return (
+
     <div className="app">
     
       <Header />
 
-      <section id="primary-content" className="sticky-wrap">
-
-        <NewButton id="new-btn" className="dash-btn" text="+ New Sticky" />
-        <SaveButton id="save-btn" className="dash-btn" text="Save Board" />
-        <ClearButton id="clear-btn" className="dash-btn" text="Clear All" />
-        <DeleteButton id="delete-btn" className="dash-btn" text="Delete All" />
-        <LoadButton id="load-btn" className="dash-btn" text="Load Board" />
-
-        <div id="sticky-notes"></div>
-        
-      </section>
+      <NoteBoard />
 
     </div>
   );
