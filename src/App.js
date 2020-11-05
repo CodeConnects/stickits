9dbfe08374aa83986f2e0072238ea8273910a122
oca <codeconnects@gmail.com>
2020-11-05 10:46:48 -0500

    added individual folders to component folder for component specific css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import NoteBoard from './components/NoteBoard';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import NoteBoard from './components/NoteBoard/NoteBoard';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+
+        <footer id="main-footer"></footer>
+    );
+}
+
+export default Footer;
