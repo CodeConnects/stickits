e17d9f89d27d8366ecc06d1b1e7c6779746314a5
oca <codeconnects@gmail.com>
2020-11-05 14:47:06 -0500

    add beginnings of state to noteBoard

--- src/components/NoteBoard/NoteBoard.js
+++ src/components/NoteBoard/NoteBoard.js
@@ -1,50 +1,58 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import Button from '../Button/Button.js';
+import StickyNote from '../StickyNote/StickyNote.js';
 
-class NoteBoard extends React.Component {
+//require("downloadjs")(data, strFileName, strMimeType);
 
-    /*
-    constructor() {
-        super();
-    };
-    */
+//const allNotes = [];
 
-    newNote() {
+const NoteBoard = () => {
+    const [list, setList] = React.useState([])
+
+    // methods to work with the notes
+    function newNote() {
         console.log("make a new sticky on this noteboard");
+        const newList = list.concat({ title: 'first new', body: 'first body', id: uuidv4() });
+        setList(newList);
     }
-    saveNotes() {
+    function saveNotes() {
         console.log("save this noteboard");
+        //download("hello world", "react-text.txt", "text/plain");
     }
-    clearNotes() {
+    function clearNotes() {
         console.log("clear all stickies on this noteboard");
     }
-    deleteNotes() {
+
+    function deleteNotes() {
         console.log("delete all stickies on this noteboard");
     }
-    loadNotes() {
+
+    function loadNotes () {
         console.log("load a saved noteboard");
     }
 
-    render() {
-        return (
+    return (
 
-            <section id="note-board" className="sticky-wrap">
+        <section id="note-board" className="sticky-wrap">
 
-                <Button onClick={this.saveNotes} id="save-btn" className="dash-btn" text="Save Board" />
+            <Button onClick={newNote} id="new-btn" className="dash-btn" text="New Sticky" />
+            <Button onClick={saveNotes} id="save-btn" className="dash-btn" text="Save Board" />
+            <Button onClick={clearNotes} id="clear-btn" className="dash-btn" text="Clear All" />
+            <Button onClick={deleteNotes} id="delete-btn" className="dash-btn" text="Delete All" />
+            <Button onClick={loadNotes} id="load-btn" className="dash-btn" text="Load Board" />
 
-                <Button onClick={this.clearNotes} id="clear-btn" className="dash-btn" text="Clear All" />
-
-                <Button onClick={this.deleteNotes} id="delete-btn" className="dash-btn" text="Delete All" />
-
-                <Button onClick={this.loadNotes} id="load-btn" className="dash-btn" text="Load Board" />
-
-                <Button onClick={this.newNote} id="new-btn" className="dash-btn" text="New Sticky" />
-
-                <div id="sticky-notes"></div>
-            
-            </section>
-        );
-    }
+            <div id="sticky-notes">
+                {
+                    list.map(note => (
+                        <StickyNote key={note.id} title={note.title} body={note.body} />
+                    ))
+                }
+            </div>
+        
+        </section>
+    );
 }
 
 export default NoteBoard;
