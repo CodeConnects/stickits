68d9c893aaecd148fdafb82f7b6b591e5635053e
oca <codeconnects@gmail.com>
2020-11-06 10:00:37 -0500

    add an incremented div css id for each sticky note

--- src/components/StickyNote/StickyNote.js
+++ src/components/StickyNote/StickyNote.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const StickyNote = props => {
 
+    // set a unique div ID for css
+    let stickID = "sticky-note-"+props.count;
+    
     return (
         
-        <div className="sticky-note">
+        <div id={stickID} className="sticky-note">
             <h2 className="sticky-title">{props.title}</h2>
             <div className="sticky-body">{props.body}</div>
         </div>
