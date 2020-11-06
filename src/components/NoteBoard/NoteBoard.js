cf78832d3902c25085bf852d1c0eb6e2faee23dd
oca <codeconnects@gmail.com>
2020-11-06 09:58:40 -0500

    fill out clear, new and delete functions

--- src/components/NoteBoard/NoteBoard.js
+++ src/components/NoteBoard/NoteBoard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import Button from '../Button/Button.js';
@@ -6,29 +6,38 @@ import StickyNote from '../StickyNote/StickyNote.js';
 
 //require("downloadjs")(data, strFileName, strMimeType);
 
-//const allNotes = [];
-
 const NoteBoard = () => {
-    const [list, setList] = React.useState([])
+    const [list, setList] = useState([])
+    const [number, setNumber] = useState(0);
 
     // methods to work with the notes
     function newNote() {
         console.log("make a new sticky on this noteboard");
-        const newList = list.concat({ title: 'first new', body: 'first body', id: uuidv4() });
+        const num = (number+1);
+        const newList = list.concat({ title: 'title #'+num, body: num+' body', count: num, uuid: uuidv4() });
         setList(newList);
+        setNumber(num);
     }
+    function deleteNotes() {
+        setList([]);
+        setNumber(0);
+    }
+
+    function clearNotes() {
+        console.log("clear all stickies on this noteboard");
+
+        const clearList = list.map( item => {
+            item['title'] = item['body'] = ' ';
+            return item;
+        });
+
+        setList(clearList);
+    }
+
     function saveNotes() {
         console.log("save this noteboard");
         //download("hello world", "react-text.txt", "text/plain");
     }
-    function clearNotes() {
-        console.log("clear all stickies on this noteboard");
-    }
-
-    function deleteNotes() {
-        console.log("delete all stickies on this noteboard");
-    }
-
     function loadNotes () {
         console.log("load a saved noteboard");
     }
@@ -38,15 +47,18 @@ const NoteBoard = () => {
         <section id="note-board" className="sticky-wrap">
 
             <Button onClick={newNote} id="new-btn" className="dash-btn" text="New Sticky" />
-            <Button onClick={saveNotes} id="save-btn" className="dash-btn" text="Save Board" />
             <Button onClick={clearNotes} id="clear-btn" className="dash-btn" text="Clear All" />
             <Button onClick={deleteNotes} id="delete-btn" className="dash-btn" text="Delete All" />
+
+            <span id="sticky-btn-spacer"></span>
+
+            <Button onClick={saveNotes} id="save-btn" className="dash-btn" text="Save Board" />
             <Button onClick={loadNotes} id="load-btn" className="dash-btn" text="Load Board" />
 
             <div id="sticky-notes">
                 {
                     list.map(note => (
-                        <StickyNote key={note.id} title={note.title} body={note.body} />
+                        <StickyNote key={note.uuid} title={note.title} body={note.body} count={note.count} />
                     ))
                 }
             </div>
